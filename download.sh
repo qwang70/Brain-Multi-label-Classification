a5db76b6434538d31890da68b8f2c31862ae1244
qwang70 <qiwen.wang@163.com>
2017-12-02 09:50:09 -0600

    add download link

---
+++ download.sh
@@ -0,0 +1,4 @@
+wget -O ./tag_name.npy 'https://cs446data.blob.core.windows.net/cs446-fall17-data/tag_name.npy'
+wget -O ./train_X.npy 'https://cs446data.blob.core.windows.net/cs446-fall17-data/train_X.npy'
+wget -O ./train_binary_Y.npy 'https://cs446data.blob.core.windows.net/cs446-fall17-data/train_binary_Y.npy'
+wget -O ./valid_test_X.npy 'https://cs446data.blob.core.windows.net/cs446-fall17-data/valid_test_X.npy'
