0982b3ce60477990e1ba3254b24dd781af8c96a2
qwang70 <qiwen.wang@163.com>
2017-12-05 23:05:37 +0000

    processing

--- label_occurence/compare.py
+++ label_occurence/compare.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 
-result = pd.read_csv('result.csv')
+result = pd.read_csv('post_result.csv')
 training = pd.read_csv('train_binary_Y.csv')
 result.columns = ['label', 'occ_result']
 training.columns = ['label', 'occ_training']
+result['perc_result']= result['occ_result']/result['occ_result'].sum()
+training['perc_training']= training['occ_training']/training['occ_training'].sum()
 df = result.merge(training, how='outer', left_on='label', right_on='label')
-print(df)
+df = df.sort_values('occ_training', ascending=False)
+print(df[['label','perc_result','perc_training']])
